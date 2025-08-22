db37cd48e1d2b10452d658c4450d51c4decb63f4
FadeevaDasha <genkovna1999@gmail.com>
2025-08-22 10:07:44 +0300

    Добавила параметезированный тест 'Получение списка питомцев по статусу'

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,6 +1,7 @@
 import allure
 import requests
 import jsonschema
+import pytest
 
 from .conftest import create_pet
 from .schemas.pet_schema import PET_SCHEMA
@@ -99,6 +100,7 @@ class TestPet:
             assert response_json['tags'] == payload['tags'], 'tags питомца не совпадает с ожидаемым'
             assert response_json['status'] == payload['status'], 'status питомца не совпадает с ожидаемым'
 
+
     @allure.title('Получение информации о питомце по ID')
     def test_get_pet_by_id(self, create_pet):
         with allure.step('Получение ID созданного питомца'):
@@ -111,11 +113,11 @@ class TestPet:
             assert response.status_code == 200, 'Код ответа не совпал с ожидаемым'
             assert response.json()['id'] == pet_id, 'id питомца не совпадает с ожидаемым'
 
+
     @allure.title('Обновление информации о питомце')
     def test_updating_information_pet(self, create_pet):
         with allure.step('Получение ID созданного питомца'):
             pet_id = create_pet['id']
-
 
         with allure.step('Подготовка данных для обновления питомца'):
             payload = {
@@ -137,11 +139,11 @@ class TestPet:
                 assert response_json['name'] == payload['name'], 'name питомца не совпадает с ожидаемым'
                 assert response_json['status'] == payload['status'], 'status питомца не совпадает с ожидаемым'
 
+
     @allure.title('Удаление питомца по ID')
     def test_delete_pet_by_id(self, create_pet):
         with allure.step('Получение ID созданного питомца'):
             pet_id = create_pet['id']
-
 
         with allure.step('Отправка запроса на удаление несуществующего питомца'):
             response = requests.delete(url=f'{BASE_URL}/pet/{pet_id}')
@@ -154,3 +156,26 @@ class TestPet:
 
         with allure.step('Проверка статуса ответа и данных питомца'):
             assert response.status_code == 404, 'Код ответа не совпал с ожидаемым'
+
+
+    @allure.title('Получение списка питомцев по статусу')
+    @pytest.mark.parametrize(
+        'status, expected_status_code',
+        [
+            ('available', 200),
+            ('pending', 200),
+            ('sold', 200),
+            (' ', 400),
+            ('test', 400)
+        ]
+    )
+    def test_get_pets_by_status(self, status, expected_status_code):
+        with allure.step(f'Отправка запроса на получение питомцев по статусу {status}'):
+            response = requests.get(f'{BASE_URL}/pet/findByStatus', params={'status': status})
+
+        with allure.step('Проверка статуса ответа и формат данных'):
+            assert response.status_code == expected_status_code
+            if response.status_code == 200:
+                assert isinstance(response.json(), list)
+            else:
+                assert response.status_code == expected_status_code
